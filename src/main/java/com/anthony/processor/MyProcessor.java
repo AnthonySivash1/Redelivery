56e4e13c25f38d05da189a202ac04df8b3cd2cd3
Anthony Sivash <anthonysivash2@gmail.com>
2018-07-08 21:07:42 -0400

    updatecommit

--- src/main/java/com/anthony/processor/MyProcessor.java
+++ src/main/java/com/anthony/processor/MyProcessor.java
@@ -3,10 +3,14 @@ package com.anthony.processor;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
+import com.anthony.exception.CamelCustomException;
+
 public class MyProcessor implements Processor {
 
     public void process(Exchange exchange) throws Exception {
-        System.out.println("hello");
+       // System.out.println("hello");
+    	System.out.println("Exception Thrown");
+        throw new CamelCustomException();
     }
 
 }
--- src/main/java/com/anthony/router/SimpleRouteBuilder.java
+++ src/main/java/com/anthony/router/SimpleRouteBuilder.java
@@ -1,15 +1,27 @@
 package com.anthony.router;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 
+import com.anthony.exception.CamelCustomException;
 import com.anthony.processor.MyProcessor;
 
 
 public class SimpleRouteBuilder extends RouteBuilder {
+	 @Override
+	    public void configure() throws Exception {
+		 onException(CamelCustomException.class).process(new Processor() {
 
-    @Override
-    public void configure() throws Exception {
-        from("file:C:/inputFolder?noop=true").process(new MyProcessor()).to("file:C:/outputFolder");
-    }
+	            public void process(Exchange exchange) throws Exception {
+	                System.out.println("handling ex");
+	            }
+	        }).log("Received body ").handled(true);
+	        from("file:C:/inputFolder?noop=true").process(new MyProcessor()).to("file:C:/outputFolder");
+	            
 
+	        from("file:C:/inbox?noop=true").process(new MyProcessor()).to("file:C:/outbox");
+	    }
+	 
 }
+	 
